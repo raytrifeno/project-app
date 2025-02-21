666a43ae665cda0ae430382a90a3c7486978017c
raytrifeno <raysurualing@gmail.com>
2025-02-22 00:10:21 +0700

    banyakk

--- script-content.js
+++ script-content.js
@@ -2,29 +2,18 @@
   let prevScrollpos = window.pageYOffset;
   const navbar = document.querySelector(".navbar");
  
-  window.onscroll = function () {
+  window.onscroll = function() {
     let currentScrollPos = window.pageYOffset;
  
     if (prevScrollpos > currentScrollPos) {
       navbar.style.top = "0";
     } else {
-      navbar.style.top = "-70px";
+      navbar.style.top = "-70px"; 
     }
     prevScrollpos = currentScrollPos;
-  };
+  }
  
   // Kode JavaScript lainnya (scrollToTop)
   function scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
- 
-  const loadMoreBtn = document.getElementById('loadMoreBtn');
- const hiddenContent = document.getElementById('hiddenContent');
- 
- loadMoreBtn.addEventListener('click', () => {
- if (hiddenContent.style.display === 'none') {
- hiddenContent.style.display = 'block';
- } else {
- hiddenContent.style.display = 'none';
- }
- });
--- script-kirim-tulisan.js
+++ script-kirim-tulisan.js
@@ -1,22 +1,22 @@
-            // Kode JavaScript untuk menyembunyikan/menampilkan navbar saat scroll
-            let prevScrollpos = window.pageYOffset;
-            const navbar = document.querySelector(".navbar");
-           
-            window.onscroll = function() {
-              let currentScrollPos = window.pageYOffset;
-           
-              if (prevScrollpos > currentScrollPos) {
-                navbar.style.top = "0";
-              } else {
-                navbar.style.top = "-70px"; 
-              }
-              prevScrollpos = currentScrollPos;
-            }
-           
-            // Kode JavaScript lainnya (scrollToTop)
-            function scrollToTop() {
-              window.scrollTo({ top: 0, behavior: "smooth" });
-            }
-            ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
-                   console.error("Error initializing CKEditor:", error);
-                 });
+             // Kode JavaScript untuk menyembunyikan/menampilkan navbar saat scroll
+             let prevScrollpos = window.pageYOffset;
+             const navbar = document.querySelector(".navbar");
+            
+             window.onscroll = function() {
+               let currentScrollPos = window.pageYOffset;
+            
+               if (prevScrollpos > currentScrollPos) {
+                 navbar.style.top = "0";
+               } else {
+                 navbar.style.top = "-70px"; 
+               }
+               prevScrollpos = currentScrollPos;
+             }
+            
+             // Kode JavaScript lainnya (scrollToTop)
+             function scrollToTop() {
+               window.scrollTo({ top: 0, behavior: "smooth" });
+             }
+             ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
+                    console.error("Error initializing CKEditor:", error);
+                  });
